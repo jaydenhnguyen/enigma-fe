55d32fb43147aee050f35100b278e9aab590c1bf
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2025-06-04 00:17:28 -0400

    :hammer: config private layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
-import { Home } from 'src/modules/home';
-import { PublicLayout } from 'src/layouts/PublicLayout';
+import { useRouter } from 'next/router';
+import { tokenManager } from '../configs';
 
-export default function Index() {
-  return (
-    <PublicLayout>
-      <Box sx={{ width: '100%' }}>
-        <Home />
-      </Box>
-    </PublicLayout>
-  );
+export default function IndexPage() {
+  const router = useRouter();
+  const isAuthenticated = tokenManager.isAuthenticated();
+
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      router.replace('/home').then();
+    } else {
+      router.replace('/login').then();
+    }
+  }, [isAuthenticated]);
+
+  return null;
 }

502174b1d5f2a76846448a799a77ac4d57b79cc7
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2025-06-03 23:24:42 -0400

    :sparkles: done authen

--- src/modules/Authentication/Login/constants/index.ts
+++ src/modules/Authentication/Login/constants/index.ts
@@ -3,7 +3,11 @@ import { LoginRequest } from '../models';
 
 export const PASSWORD_LENGTH = 8;
 
-export const LOGIN_FORM_FIELDS: { [K in keyof LoginRequest]: Path<LoginRequest> } = {
+export const LOGIN_FORM_FIELD_NAMES: { [K in keyof LoginRequest]: Path<LoginRequest> } = {
   email: 'email',
   password: 'password',
 } as const;
+
+export const NOTIFY_MESSAGES = {
+  ERROR: 'Email or password is not correct!',
+};

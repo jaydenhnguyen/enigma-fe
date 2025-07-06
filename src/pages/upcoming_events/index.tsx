46a8407a913a3aa0dab1ccba3873174775513f9e
Dmytro Litvinov <dmlitvinov99@gmail.com>
2025-07-06 19:02:56 -0400

    feat: add EventDetailPopUp component and keep AboutUs as is

--- src/pages/upcoming_events/index.tsx
+++ src/pages/upcoming_events/index.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
 import Head from 'next/head';
 import { PrivateLayout } from 'src/layouts';
+import { EmptyModule } from "src/modules/PopUp";
+import EventDetailPopUp from "src/components/EventDetailPopUp";
 
 export default function UpcomingEventsPage(): React.ReactElement {
+  const [open, setOpen] = React.useState(false);
+  const [selectedEvent, setSelectedEvent] = React.useState<any>(null);
+
+  const handleOpen = () => {
+    setSelectedEvent({
+      clientName: "Kevin Dohery",
+      phone: "067999777888",
+      email: "dummy@email.com",
+    });
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSave = () => {
+    console.log('Saved!', selectedEvent);
+    setOpen(false);
+  };
+
   return (
     <>
       <Head>
@@ -10,8 +33,21 @@ export default function UpcomingEventsPage(): React.ReactElement {
       </Head>
 
       <div>this is Upcoming Events page</div>
+      
+      <button onClick={handleOpen}>Open PopUp</button>
+
+      <EmptyModule
+        isOpen={open}
+        onClose={handleClose}
+        onSave={handleSave}
+        title="Event Details"
+        width="1200px"
+      >
+        <EventDetailPopUp event={selectedEvent} />
+      </EmptyModule>
     </>
   );
 }
 
-UpcomingEventsPage.getLayout = (page: React.ReactElement) => <PrivateLayout>{page}</PrivateLayout>;
+UpcomingEventsPage.getLayout = (page: React.ReactElement) =>
+  <PrivateLayout>{page}</PrivateLayout>;
